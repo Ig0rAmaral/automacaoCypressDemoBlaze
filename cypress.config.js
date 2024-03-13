28b4dd7ffdf04961855640d7ba9e327e6efee41f
Igor Carvalho Amaral de Santana <109313097+Ig0rAmaral@users.noreply.github.com>
2024-03-13 16:41:01 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://demoblaze.com/index.html',
+    baseUrl: 'https://demoblaze.com',
     failOnStatusCode: false,
       "video": false,
       "chromeWebSecurity": false,
